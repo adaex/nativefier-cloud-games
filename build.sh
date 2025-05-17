81a5c29f804d16ccb1744d51d4676bd8edffbd79
杨涛 <adaex.yang@bytedance.com>
2025-05-17 20:47:24 +0800

    修正构建脚本中的图标路径，确保正确引用图标文件

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 nativefier "https://www.migufun.com/middleh5/ucenter" dist \
   --name "MiGuFun" \
-  --icon "./icon.icns" \
+  --icon "icon.icns" \
   --platform "darwin" \
   --arch "arm64" \
   --single-instance \
   --fast-quit \
+  --verbose \
   --electron-version "36.2.0" \
