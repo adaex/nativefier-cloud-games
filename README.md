049ea848a945a48c91b18d65d533ae9db5952221
杨涛 <adaex.yang@bytedance.com>
2025-05-17 14:54:47 +0800

    添加 .gitignore 文件，更新 README.md，新增构建脚本 build.sh 和图标文件 icon.icns

---
+++ README.md
@@ -0,0 +1,7 @@
+## 查看 Electron 版本
+
+https://releases.electronjs.org/release?channel=stable
+
+## 查看 Nativefier API
+
+https://github.com/nativefier/nativefier/blob/master/API.md
---
+++ build.sh
@@ -0,0 +1,8 @@
+nativefier "https://www.migufun.com/middleh5/ucenter" dist \
+  --name "MiGuFun" \
+  --icon "./icon.icns" \
+  --platform "darwin" \
+  --arch "arm64" \
+  --single-instance \
+  --fast-quit \
+  --electron-version "36.2.0" \
