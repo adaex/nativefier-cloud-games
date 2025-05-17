df46dc31450fe7d29f8cc0f68d4dffd0d4e122d8
杨涛 <adaex.yang@bytedance.com>
2025-05-18 01:07:49 +0800

    更新构建脚本以使用正确的图标路径，并添加新的图标文件

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
 nativefier "https://www.migufun.com/middleh5/ucenter" dist \
   --name "MiGuFun" \
-  --icon "icon.icns" \
+  --icon "icons/migufun.icns" \
+  --platform "darwin" \
+  --arch "arm64" \
+  --single-instance \
+  --fast-quit \
+  --verbose \
+  --electron-version "36.2.0" \
+
+
+nativefier "https://mhyy.mihoyo.com" dist \
+  --name "miHoYo" \
+  --icon "icons/mihoyo.icns" \
   --platform "darwin" \
   --arch "arm64" \
   --single-instance \
