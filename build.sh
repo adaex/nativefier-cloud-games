2e310ad9147f088b0bf16580025b6a860d7f8351
杨涛 <adaex.yang@bytedance.com>
2025-05-18 01:34:15 +0800

    更新构建脚本以添加 miHoYo 的书签菜单，并修正相关链接

--- build.sh
+++ build.sh
@@ -9,9 +9,10 @@ nativefier "https://www.migufun.com/middleh5/ucenter" dist \
   --electron-version "36.2.0" \
 
 
-nativefier "https://mhyy.mihoyo.com" dist \
+nativefier "https://ys.mihoyo.com/cloud/#/" dist \
   --name "miHoYo" \
   --icon "icons/mihoyo.icns" \
+  --bookmarks-menu "menus/mihoyo.json" \
   --platform "darwin" \
   --arch "arm64" \
   --single-instance \
