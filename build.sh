0ce95a9cd80e76edc2dbab093220b0e29a2dd31c
杨涛 <adaex.yang@bytedance.com>
2025-06-28 11:11:52 +0800

    更新构建脚本以自动获取最新的 Electron 版本，并移除硬编码的版本号

--- build.sh
+++ build.sh
@@ -1,13 +1,22 @@
+#!/bin/bash
+
+# Automatically get the latest Electron version
+# https://releases.electronjs.org/release?channel=stable
+echo "正在获取最新的 Electron 版本..."
+ELECTRON_VERSION=$(npm view electron version --registry https://registry.npmmirror.com)
+
+echo "使用 Electron 版本: $ELECTRON_VERSION"
+
 nativefier "https://www.migufun.com/middleh5/ucenter" dist \
   --name "MiGuFun" \
   --icon "icons/migufun.icns" \
   --bookmarks-menu "menus/migufan.json" \
   --platform "darwin" \
   --arch "arm64" \
+  --electron-version "$ELECTRON_VERSION" \
   --single-instance \
   --fast-quit \
-  --verbose \
-  --electron-version "36.4.0" \
+  # --verbose \
 
 
 nativefier "https://ys.mihoyo.com/cloud/#/" dist \
@@ -16,7 +25,7 @@ nativefier "https://ys.mihoyo.com/cloud/#/" dist \
   --bookmarks-menu "menus/mihoyo.json" \
   --platform "darwin" \
   --arch "arm64" \
+  --electron-version "$ELECTRON_VERSION" \
   --single-instance \
   --fast-quit \
-  --verbose \
-  --electron-version "36.4.0" \
+  # --verbose \
