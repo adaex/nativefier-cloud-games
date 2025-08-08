a538ec0c5d486eb35c1a99ea8e70c6389e89c003
杨涛 <adaex.yang@bytedance.com>
2025-08-08 21:16:03 +0800

    添加构建脚本以自动获取最新的 Electron 版本并构建 MiGuFun 和 miHoYo 应用

---
+++ build_bak_250808.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Automatically get the latest Electron version
+# https://releases.electronjs.org/release?channel=stable
+echo "正在获取最新的 Electron 版本..."
+ELECTRON_VERSION=$(npm view electron version --registry https://registry.npmmirror.com)
+
+echo "使用 Electron 版本: $ELECTRON_VERSION"
+
+nativefier "https://www.migufun.com/middlepc/ucenter" dist \
+  --name "MiGuFun" \
+  --icon "icons/migufun.icns" \
+  --bookmarks-menu "menus/migufan.json" \
+  --platform "darwin" \
+  --arch "arm64" \
+  --electron-version "$ELECTRON_VERSION" \
+  --single-instance \
+  --fast-quit \
+  # --verbose \
+
+
+nativefier "https://ys.mihoyo.com/cloud/#/" dist \
+  --name "miHoYo" \
+  --icon "icons/mihoyo.icns" \
+  --bookmarks-menu "menus/mihoyo.json" \
+  --platform "darwin" \
+  --arch "arm64" \
+  --electron-version "$ELECTRON_VERSION" \
+  --single-instance \
+  --fast-quit \
+  # --verbose \
